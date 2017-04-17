7566fcf136272678d6e624ae53954d8f218d6200
Nataniel Paiva <nataniel.paiva@gmail.com>
2017-04-17 19:10:20 -0300

    Subindo projeto

---
+++ src/main/java/spring/cassandra/rest/Application.java
@@ -0,0 +1,50 @@
+package spring.cassandra.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ *
+ * @author mais
+ */
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+//    public static void main(String[] args) {
+//        SpringApplication.run(Application.class, args);
+//    }
+    public static void main(String[] args) {
+        SpringApplication.run(applicationClass, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(applicationClass);
+    }
+
+    private static Class<Application> applicationClass = Application.class;
+
+    @Bean
+    public CorsFilter corsFilter() {
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true); // you USUALLY want this
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("DELETE");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
+}
---
+++ src/main/java/spring/cassandra/rest/controller/CustomerController.java
@@ -0,0 +1,52 @@
+package spring.cassandra.rest.controller;
+
+import com.datastax.driver.core.utils.UUIDs;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import spring.cassandra.rest.entity.Customer;
+import spring.cassandra.rest.repository.CustomerRepository;
+
+/**
+ *
+ * @author mais
+ */
+@RestController
+public class CustomerController {
+    
+    @Autowired
+    CustomerRepository customerRepository;
+    
+    @RequestMapping(value = "/customer", method = RequestMethod.GET)
+    public List<Customer> list() {
+        return (List<Customer>) customerRepository.findAll();
+    }
+    
+    @RequestMapping(value = "/customer", method = RequestMethod.POST)
+    public Customer save(@RequestBody Customer customer) {
+        customer.setId(UUIDs.timeBased().toString());
+        return customerRepository.save(customer);
+    }
+    
+    @RequestMapping(value = "/customer", method = RequestMethod.PUT)
+    public Customer update(@RequestBody Customer customer) {
+        return customerRepository.save(customer);
+    }
+    
+    @RequestMapping(value = "/customer/{id}", method = RequestMethod.GET)
+    public Customer getById(@PathVariable String id) {
+        Customer customer = customerRepository.findOne(id);
+        return customer;
+    }
+    
+    @RequestMapping(value = "/customer/{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable String id) {
+        customerRepository.delete(id);
+    }
+    
+}
